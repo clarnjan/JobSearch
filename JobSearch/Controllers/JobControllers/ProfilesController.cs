3c3d316f130d20cdeeeafcb4e1c858aa530e3fa4
Damjan <damjananakiev@gmail.com>
2020-07-17 11:00:40 +0200

    initial with login

---
+++ JobSearch/Controllers/JobControllers/ProfilesController.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+using JobSearch.Models.JobModels;
+
+namespace JobSearch.Controllers.JobControllers
+{
+    public class ProfilesController : Controller
+    {
+        private EmploymentContext db = new EmploymentContext();
+
+        // GET: Profiles
+        public ActionResult Index()
+        {
+            return View(db.Profiles.ToList());
+        }
+
+        // GET: Profiles/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Profile profile = db.Profiles.Find(id);
+            if (profile == null)
+            {
+                return HttpNotFound();
+            }
+            return View(profile);
+        }
+
+        // GET: Profiles/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Profiles/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,Username,Password,eAddress,Name,Surname,Role")] Profile profile)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Profiles.Add(profile);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(profile);
+        }
+
+        // GET: Profiles/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Profile profile = db.Profiles.Find(id);
+            if (profile == null)
+            {
+                return HttpNotFound();
+            }
+            return View(profile);
+        }
+
+        // POST: Profiles/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,Username,Password,eAddress,Name,Surname,Role")] Profile profile)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(profile).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(profile);
+        }
+
+        // GET: Profiles/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Profile profile = db.Profiles.Find(id);
+            if (profile == null)
+            {
+                return HttpNotFound();
+            }
+            return View(profile);
+        }
+
+        // POST: Profiles/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Profile profile = db.Profiles.Find(id);
+            db.Profiles.Remove(profile);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [AllowAnonymous]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Login(Profile profile)
+        {
+            var UserName = db.Profiles.Where(x => x.Username == profile.Username).FirstOrDefault();
+            var UserPassword = db.Profiles.Where(x => x.Password == profile.Password).FirstOrDefault();
+            if (ModelState.IsValid)
+            {
+                if (UserName == null || UserPassword == null)
+                {
+                    ModelState.AddModelError("", "Invalid username or password.");
+                    return View();
+                }
+                var id = db.Profiles.Where(x => x.Username == profile.Username && x.Password == profile.Password).FirstOrDefault().Id;
+                var role = db.Profiles.Where(x => x.Id == id).FirstOrDefault();
+                
+            }
+
+            return RedirectToAction("Index", "Home");
+        }
+    }
+}
