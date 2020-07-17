4b71f4e0836189a2379212d0098b96c142bfcee6
Katerina <golcevska.katerina@yahoo.com>
2020-07-17 22:00:32 +0200

    portfolio and job

--- JobSearch/Controllers/JobControllers/ProfilesController.cs
+++ JobSearch/Controllers/JobControllers/ProfilesController.cs
@@ -51,9 +51,31 @@ namespace JobSearch.Controllers.JobControllers
         {
             if (ModelState.IsValid)
             {
-                db.Profiles.Add(profile);
-                db.SaveChanges();
-                return RedirectToAction("Index");
+                var user = db.Profiles.Where(x => x.Username == profile.Username).FirstOrDefault();
+                var user2 = db.Profiles.Where(x => x.eAddress == profile.eAddress).FirstOrDefault();
+                if (user == null && user2==null)
+                {
+                    db.Profiles.Add(profile);
+                    db.SaveChanges();
+                    HttpCookie userInfo = new HttpCookie("userInfo");
+                    userInfo["UserName"] = user.Username;
+                    userInfo["Id"] = user.Id.ToString();
+                    userInfo["Role"] = user.Role.ToString();
+                    userInfo["Name"] = user.Name;
+                    userInfo["Surname"] = user.Surname;
+                    userInfo["eAddress"] = user.eAddress;
+                    userInfo.Expires.Add(new TimeSpan(0, 1, 0));
+                    Response.Cookies.Add(userInfo);
+                    return RedirectToAction("Index");
+                }
+                else
+                {
+                    if (user != null)
+                        ModelState.AddModelError("", "Username allready exists");
+                    else if (user2 != null)
+                        ModelState.AddModelError("", "Email allready exists");
+                    return View();
+                }
             }
 
             return View(profile);
@@ -134,21 +156,35 @@ namespace JobSearch.Controllers.JobControllers
         [ValidateAntiForgeryToken]
         public async Task<ActionResult> Login(Profile profile)
         {
-            var UserName = db.Profiles.Where(x => x.Username == profile.Username).FirstOrDefault();
-            var UserPassword = db.Profiles.Where(x => x.Password == profile.Password).FirstOrDefault();
-            if (ModelState.IsValid)
+            var user = db.Profiles.Where(x => x.Username == profile.Username && x.Password == profile.Password).FirstOrDefault();
+            if (user == null)
             {
-                if (UserName == null || UserPassword == null)
-                {
-                    ModelState.AddModelError("", "Invalid username or password.");
-                    return View();
-                }
-                var id = db.Profiles.Where(x => x.Username == profile.Username && x.Password == profile.Password).FirstOrDefault().Id;
-                var role = db.Profiles.Where(x => x.Id == id).FirstOrDefault();
-                
+                ModelState.AddModelError("", "Invalid username or password.");
+                return View();
             }
+            //System.Diagnostics.Debug.WriteLine(profile.Username);
+            HttpCookie userInfo = new HttpCookie("userInfo");
+            userInfo["UserName"] = user.Username;
+            userInfo["Id"] = user.Id.ToString();
+            userInfo["Role"] = user.Role.ToString();
+            userInfo["Name"] = user.Name;
+            userInfo["Surname"] = user.Surname;
+            userInfo["eAddress"] = user.eAddress;
+            userInfo.Expires.Add(new TimeSpan(0, 1, 0));
+            Response.Cookies.Add(userInfo);
 
-            return RedirectToAction("Index", "Home");
+            return RedirectToAction("Index", "Jobs");
+            
+        }
+        public ActionResult LogOff()
+        {
+            if (Request.Cookies["userInfo"] != null)
+            {
+                var c = new HttpCookie("userInfo");
+                c.Expires = DateTime.Now.AddDays(-1);
+                Response.Cookies.Add(c);
+            }
+            return RedirectToAction("Index", "Jobs");
         }
     }
 }
